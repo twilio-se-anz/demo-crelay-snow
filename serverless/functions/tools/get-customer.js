03b7705f1c12b1bb0342277f0d216d0e1f7d52d2
Des Hartman <des@deshartman.com>
2024-10-30 15:36:00 +1100

    Stripped version to avoid privacy issues

--- serverless/functions/tools/get-customer.js
+++ serverless/functions/tools/get-customer.js
@@ -3,7 +3,6 @@ exports.handler = async function (context, event, callback) {
 
   try {
     // Extract phone-number from the event object
-    // let caller = event.call.from_number; // This is for RetellAI
     let caller = event.from;
     if (!caller) {
       throw new Error('[getCustomer] phone-number is missing from the event object');
@@ -12,20 +11,12 @@ exports.handler = async function (context, event, callback) {
     console.log(`[getCustomer] Phone number provided:`, caller);
 
     // You would replace this with actual logic (e.g., database lookup)
-    const customerDatabase = {
-      '+61401277XXX': {
-        firstname: "Des",
-        lastname: "Hartman"
-      }
-    };
-
-    const customerData = customerDatabase[caller];
-    console.log(`[getCustomer] customer returned:`, customerData);
-    if (customerData) {
-      return callback(null, customerData);
-    } else {
-      return callback(null);
+    const customerData = {
+      firstname: "Des",
+      lastname: "Hartman"
     }
+    console.log(`[getCustomer] customer returned:`, customerData);
+    return callback(null, customerData);
   } catch (error) {
     return callback(`[getCustomer] Error: ${error}`);
   }
